46b9a67ae48dca0dfd43f807f710fa3bd860ca0e
TheComputer <thecomputer@The-Spare-MacBook-Air.local>
2025-04-22 10:04:49 -0600

    test results added to readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # fuzzy-succotash-tui
+
+Just a little test to see if calculating,
+
+- the 50000th number of the Fibonacci sequence
+- Pi to 50000 iterations
+- the number of Primes in 50000
+
+is faster calling sequentally or concurrently.
+
+The result was surprising:
+
+[GIN] 2025/04/22 - 09:46:06 | 200 |   29.021334ms |             ::1 | GET      "/calc/all/inline"
+[GIN] 2025/04/22 - 09:46:11 | 200 |     32.7945ms |             ::1 | GET      "/calc/all/concurrent"
+
+until considering the cost to setup the Go routines and wait group!
--- server/routes.go
+++ server/routes.go
@@ -3,6 +3,8 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"runtime"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -69,6 +71,71 @@ func loadTestingRoutes(r *gin.Engine) {
 		c.JSON(http.StatusOK, gin.H{
 			"duration": duration.String(),
 			"result":   fmt.Sprintf("%d primes", result), // Return count
+		})
+	})
+
+	r.GET("/calc/all/inline", func(c *gin.Context) {
+		start := time.Now()
+
+		nth := 50000
+		sequence := computeFibonacci(nth)
+
+		limit := 50000
+		result := countPrime(limit)
+
+		iterations := 50000
+		pi := calculatePi(iterations)
+
+		duration := time.Since(start)
+
+		c.JSON(http.StatusOK, gin.H{
+			"duration":  duration.String(),
+			"Fibonacci": fmt.Sprintf("%s...", sequence.String()[:10]),
+			"Pi":        fmt.Sprintf("Pi approximation at %d iterations: %.15f\n", iterations, pi),
+			"Primes":    fmt.Sprintf("%d primes", result),
+		})
+	})
+
+	type allResponse struct {
+		Fibonacci string `json:"fibonacci"`
+		Pi        string `json:"pi"`
+		Primes    string `json:"primes"`
+	}
+
+	r.GET("/calc/all/concurrent", func(c *gin.Context) {
+		start := time.Now()
+		runtime.GOMAXPROCS(runtime.NumCPU())
+		var response allResponse
+		var wg sync.WaitGroup
+		wg.Add(3)
+
+		nth := 50000
+		go func() {
+			defer wg.Done()
+			result := computeFibonacci(nth)
+			response.Fibonacci = fmt.Sprintf("%s...", result.String()[:10])
+		}()
+
+		limit := 50000
+		go func() {
+			defer wg.Done()
+			result := countPrime(limit)
+			response.Primes = fmt.Sprintf("%d primes", result)
+		}()
+
+		iterations := 50000
+		go func() {
+			defer wg.Done()
+			result := calculatePi(iterations)
+			response.Pi = fmt.Sprintf("Pi approximation at %d iterations: %.15f", iterations, result)
+		}()
+
+		wg.Wait()
+		duration := time.Since(start)
+
+		c.JSON(http.StatusOK, gin.H{
+			"duration": duration.String(),
+			"results":  response,
 		})
 	})
 }
